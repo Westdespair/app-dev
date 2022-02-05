30109a99e8bbd8a0492d06bb3c9955a78befec83
Sindre Skorpen <64530905+Westdespair@users.noreply.github.com>
2022-02-05 21:55:27 +0100

    Completed week 1 exercise 2 -Got an overview on the different classes -Changed the greeting message -Added an user

--- example-01-HelloSpring/src/main/java/no/ntnu/hello/GreetingController.java
+++ example-01-HelloSpring/src/main/java/no/ntnu/hello/GreetingController.java
@@ -11,7 +11,7 @@ public class GreetingController {
 
     @GetMapping("/hello")
     public String greeting() {
-        return "Hello, World";
+        return "Thanks for checking out this website :)";
     }
 }
 
